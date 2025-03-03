f42e6c1080d350d03657c2b7c475a8dcea30fb8e
Miguel <miguelpoleto5@gmail.com>
2025-03-03 15:35:59 -0300

    User create and Hash password

--- src/main/java/br/com/todolist/todolist/user/UserModel.java
+++ src/main/java/br/com/todolist/todolist/user/UserModel.java
@@ -1,12 +1,34 @@
 package br.com.todolist.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(name = "username", unique = true)
     private String username;
+
+    @Column(name = "name")
     private String name;
+    
+    @Column(name = "password")
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
